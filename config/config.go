677ac4a208260433e866ee2ba74ffa049f842da5
tx2017 <tx2017@tx2017deMacBook-Pro.local>
2018-04-16 14:37:47 +0800

    configfile

--- config/config.go
+++ config/config.go
@@ -21,21 +21,21 @@ staging:
   password: n[h^nM06
   host: test.tongxinyiliao.com
   dbport: 3306
-  redis_host: '127.0.0.1'
+  redis_host: 'dicomuptest.tongxinyiliao.com'
   redis_port: 6379
-  redis_pwd: ''
+  redis_pwd: 'b1e41f0f3dc551703dc51c64d1970fe5'
   redis_db: 3
 
 production:
   port: :9091
-  database: txhims_staging
-  username: root
-  password: n[h^nM06
-  host: test.tongxinyiliao.com
+  database: txhims_production
+  username: pro_txdb
+  password: Tongxin2014
+  host: rdsarjziaiq3e2a.mysql.rds.aliyuncs.com
   dbport: 3306
-  redis_host: '127.0.0.1'
+  redis_host: 'r-2ze1efa5198c21c4.redis.rds.aliyuncs.com'
   redis_port: 6379
-  redis_pwd: ''
+  redis_pwd: 'r-2ze1efa5198c21c4:TongXin2017'
   redis_db: 3
 
 
